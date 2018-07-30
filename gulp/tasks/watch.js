f1429b98703298d9f2e793c0fdf75dc96f389095
彭津雲 <pxc1861@hotmail.com>
2018-07-30 19:59:10 +0800

    时间的轨迹节点

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch',function(){
+
+    browserSync.init({
+    notify: false,
+    server: {
+        baseDir: './'
+    }
+})
+
+    watch(['index.html','timeline.json'],function(){
+        gulp.start('html');
+    })
+
+    watch('assets/styles/**/*.css',function(){
+        gulp.start('cssInject');
+    })
+})
+
+gulp.task("cssInject", ['styles'], function () {
+    return gulp.src('./assets/temp/styles/styles.css')
+        .pipe(browserSync.stream())
+})
+
+gulp.task('html',function(){
+    browserSync.reload();
+})
